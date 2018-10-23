1ed6e907406353ad360717959c38221790e89cf8
Jason Kerney <Jason@TeamKerney.com>
2018-10-22 21:18:42 -0700

    added more pizza builder testing

--- test/pizzaBuilder.test.js
+++ test/pizzaBuilder.test.js
@@ -3,7 +3,8 @@
 const mockery = require('mockery');
 const sinon = require('sinon');
 const { assert } = require('chai');
-const {mockerySetup} = require('./test-utils/mockerySetup');
+const { mockerySetup } = require('./test-utils/mockerySetup');
+const signet = require('../app/types');
 
 describe('Pizza Builder', function () {
     let pizzaBuilder;
@@ -47,20 +48,34 @@ describe('Pizza Builder', function () {
         pizzaBuilder = require('../app/pizzaBuilder');
     });
 
-    function reportOnTopping(name) {
-        let toppingSpy = toppingSpys[name];
+    let isDefined = signet.isTypeOf('not<undefined>');
+    let isFunction = signet.isTypeOf('function');
+
+
+    function reportOnPossibleSpy(possilbeSpy) {
+        if(isDefined(possilbeSpy.callCount)) {
+            return possilbeSpy.callCount;
+        }
+        if(isFunction(possilbeSpy)) {
+            return possilbeSpy();
+        }
+        
+        throw new Error('unknown type');
+    }
+
+    function reportOnTopping(toppingSpy, altName) {
         let r = {
-            name: toppingSpy.__name,
-            'getTopping called': toppingSpy.getTopping.callCount,
-            'getAmmount called': toppingSpy.getAmmount.callCount,
-            'hasAny called': toppingSpy.hasAny.callCount
+            name: Boolean(toppingSpy.__name) ? toppingSpy.__name : altName,
+            'getTopping': reportOnPossibleSpy(toppingSpy.getTopping),
+            'getAmmount': reportOnPossibleSpy(toppingSpy.getAmmount),
+            'hasAny': reportOnPossibleSpy(toppingSpy.hasAny)
         };
 
         return JSON.stringify(r);
     };
 
-    function reportOnAllToppings(){
-        return Object.keys(toppingSpys).map(name => reportOnTopping(name));
+    function reportOnAllToppings(topping) {
+        return Object.keys(topping).map(name => reportOnTopping(topping[name], name));
     }
 
     it('builds toppings', function () {
@@ -85,31 +100,38 @@ describe('Pizza Builder', function () {
         let r = p.getTotal();
 
         assert.approximately(r, 24.94, .02);
-        let s = reportOnAllToppings();
+        let s = reportOnAllToppings(toppingSpys);
 
-        assert.equal(s[0], '{"name":"pepperoni Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
-        assert.equal(s[1], '{"name":"sausage Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
-        assert.equal(s[2], '{"name":"pineapple Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
-        assert.equal(s[3], '{"name":"canadian bacon Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
-        assert.equal(s[4], '{"name":"bacon Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
-        assert.equal(s[5], '{"name":"new testing topping Mock","getTopping called":0,"getAmmount called":1,"hasAny called":0}');
+        assert.equal(s[0], '{"name":"pepperoni Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
+        assert.equal(s[1], '{"name":"sausage Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
+        assert.equal(s[2], '{"name":"pineapple Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
+        assert.equal(s[3], '{"name":"canadian bacon Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
+        assert.equal(s[4], '{"name":"bacon Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
+        assert.equal(s[5], '{"name":"new testing topping Mock","getTopping":0,"getAmmount":1,"hasAny":0}');
     });
 
-    it('delivers the pizza', function(){
+    it('delivers the pizza', function () {
         let p = pizzaBuilder();
 
         let r = p.deliver();
 
         assert.equal(r, 'Presto! Delivered to your door.');
 
-        let s = reportOnAllToppings();
+        let s = reportOnAllToppings({
+            'pepperoni': p['pepperoni'],
+            'sausage': p['sausage'],
+            'pineapple': p['pineapple'],
+            'canadian bacon': p['canadian bacon'],
+            'bacon': p['bacon'],
+            'new testing topping': p['new testing topping']
+        });
 
-        assert.equal(s[0], '{"name":"pepperoni Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
-        assert.equal(s[1], '{"name":"sausage Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
-        assert.equal(s[2], '{"name":"pineapple Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
-        assert.equal(s[3], '{"name":"canadian bacon Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
-        assert.equal(s[4], '{"name":"bacon Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
-        assert.equal(s[5], '{"name":"new testing topping Mock","getTopping called":1,"getAmmount called":1,"hasAny called":1}');
+        assert.equal(s[0], '{"name":"pepperoni","getTopping":"pepperoni Mock","getAmmount":-0.01,"hasAny":true}');
+        assert.equal(s[1], '{"name":"sausage","getTopping":"sausage Mock","getAmmount":-0.01,"hasAny":true}');
+        assert.equal(s[2], '{"name":"pineapple","getTopping":"pineapple Mock","getAmmount":-0.01,"hasAny":true}');
+        assert.equal(s[3], '{"name":"canadian bacon","getTopping":"canadian bacon Mock","getAmmount":-0.01,"hasAny":true}');
+        assert.equal(s[4], '{"name":"bacon","getTopping":"bacon Mock","getAmmount":-0.01,"hasAny":true}');
+        assert.equal(s[5], '{"name":"new testing topping","getTopping":"new testing topping Mock","getAmmount":-0.01,"hasAny":true}');
     });
 
     afterEach(function () {
